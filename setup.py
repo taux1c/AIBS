687d8200a75df4be190fc93e51400a9628240d31
Taux1c <24677318+taux1c@users.noreply.github.com>
2023-11-22 15:47:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='AIBS',
-    version='0.0.1',
+    version='0.0.01',
     packages=['APP'],
     url='https://github.com/taux1c/AIBS',
     license='MIT',
