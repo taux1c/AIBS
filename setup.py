9bacba8707169aecb2f2b8656e38031513744972
Juan Da Don <_quentin_@live.com>
2023-11-24 10:59:10 -0500

    Fixed Download.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='AIBS',
-    version='0.0.62',
+    version='0.0.63',
     packages=['APP','APP.utils', 'APP.scrapers', 'APP.models'],
     url='https://github.com/taux1c/AIBS',
     license='MIT',
